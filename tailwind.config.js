85c9c65e224ba99bfb50b84392872e9bf121724c
tara970 <tarajef120@gmail.com>
2025-04-19 02:28:51 +0330

    my style

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // مسیر فایل‌های ریکت
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
